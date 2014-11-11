4c1524d352848790eb6bb4d18eb639495eff1d25
David <zdwloveschina@163.com>
2014-11-12 00:04:05 +0800

    add shell to set gopath auto

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "setting go path "
+currentPath=`pwd`
+export GOPATH="${currentPath}"
+export PATH="$PATH:$GOPATH/bin"
+echo "setted go path"
+
+echo "when you shut down this go server you "
+echo "should copy: export GOPATH=${currentPath}"
+echo "         and export PATH=\$PATH:\$GOPATH/bin"
+echo "to you terminal"
+
+echo "starting go server...."
+go run ${currentPath}/start.go
